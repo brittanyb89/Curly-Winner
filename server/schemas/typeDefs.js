8e67332cc570cf2f5468dd525d9eb3b9810b633c
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-03-18 16:29:28 -0500

    Define necessary query and mutation types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  # Define Query type: me (returns a User type)
+  type Query {
+    "Get the logged in user"
+    me: User
+  }
+  # Define Mutation type: login (accepts an email and password as parameters; returns an Auth type)
+  type Mutation {
+    "Login user"
+    login(email: String!, password: String!): Auth
+    # Define Mutation type: addUser (accepts a username, email, and password as parameters; returns an Auth type)
+    "Add user"
+    addUser(username: String!, email: String!, password: String!): Auth
+    # Define Mutation type: saveBook (accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type)
+    "Save book"
+    saveBook(
+      authors: [String]!
+      description: String!
+      title: String!
+      bookId: String!
+      image: String!
+      link: String!
+    ): User
+    # Define Mutation type: removeBook (accepts a book's bookId as a parameter; returns a User type)
+    "Remove book"
+    removeBook(bookId: String!): User
+  }
+  # Define user type: _id, username, email, bookCount, savedBooks (an array of the Book type)
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]!
+  }
+  # Define book type: bookId, authors, description, title, image, link
+  type Book {
+    bookId: String!
+    authors: [String]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+  # Define auth type: token, user (a User type)
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+export default typeDefs;
