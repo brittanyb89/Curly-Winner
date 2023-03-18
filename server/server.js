ab3cec2e5b3a748b25d82cffc0c0fa436675a90a
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-03-18 15:20:19 -0500

    Tool apollo and apply to express as middleware

--- server/server.js
+++ server/server.js
@@ -19,12 +19,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // TODO: if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-app.use(routes);
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Call the async function to start the Apollo server
+startApolloServer(typeDefs, resolvers);
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
+
+// app.use(routes);
+
+// db.once("open", () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
