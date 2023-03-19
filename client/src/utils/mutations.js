37adcf69eff506d004f4880d1b57c9587de7d6ba
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-03-18 19:06:11 -0500

    Add mutations using apollo server

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,85 @@
+import { gql } from "@apollo/client";
+
+// Create login_user that will execute the loginUser mutation set up using Apollo Server.
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+// Create add_user that will execute the addUser mutation set up using Apollo Server.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+// Create save_book that will execute the saveBook mutation set up using Apollo Server.
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// Create remove_book that will execute the removeBook mutation set up using Apollo Server.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
